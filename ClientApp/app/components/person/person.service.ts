f1af431a512eb225d20646b8d34b75df5970a7fb
Developer <dev@AdminisatorsMBP.fritz.box>
2017-06-02 23:07:58 +0200

    person service added

---
+++ ClientApp/app/components/person/person.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+import {Person} from './person';
+//import { personen } from './person-search.component';
+
+@Injectable()
+export class PersonService {
+  getPersonen(): void {} //stub
+}
